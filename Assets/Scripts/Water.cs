34004be0fb3219db046acaed24e215f381ee88ae
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-06-10 23:57:36 -0700

    Add empty script for water plane gen

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Water : MonoBehaviour {
+
+    public int quadRes = 1;
+
+    void OnEnable() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
