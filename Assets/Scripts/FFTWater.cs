c0c6d557ec3bcee4ddabbd8f2366a0eb15a16b72
Garrett Gunnell <gunnellg@oregonstate.edu>
2023-07-31 18:12:29 -0700

    Duplicate and delete most of water logic

---
+++ Assets/Scripts/FFTWater.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.Marshal;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class FFTWater : MonoBehaviour {
+    public Shader waterShader;
+
+    public Atmosphere atmosphere;
+
+    public int planeLength = 10;
+    public int quadRes = 10;
+
+    private Camera cam;
+
+    private Material waterMaterial;
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private Vector3[] normals;
+    
+    [Range(0.0f, 5.0f)]
+    public float normalStrength = 1;
+
+    // Shader Settings
+    [ColorUsageAttribute(false, true)]
+    public Color ambient;
+
+    [ColorUsageAttribute(false, true)]
+    public Color diffuseReflectance;
+
+    [ColorUsageAttribute(false, true)]
+    public Color specularReflectance;
+
+    [Range(0.0f, 10.0f)]
+    public float shininess = 1.0f;
+    [Range(0.0f, 5.0f)]
+    public float specularNormalStrength = 1.0f;
+
+    [ColorUsageAttribute(false, true)]
+    public Color fresnelColor;
+
+    public bool useTextureForFresnel = false;
+    public Texture environmentTexture;
+
+    [Range(0.0f, 1.0f)]
+    public float fresnelBias = 0.0f;
+    [Range(0.0f, 3.0f)]
+    public float fresnelStrength = 1.0f;
+    [Range(0.0f, 20.0f)]
+    public float fresnelShininess = 5.0f;
+
+    [Range(0.0f, 5.0f)]
+    public float fresnelNormalStrength = 1;
+
+    [ColorUsageAttribute(false, true)]
+    public Color tipColor;
+    [Range(0.0f, 5.0f)]
+    public float tipAttenuation = 1.0f;
+
+    private void CreateWaterPlane() {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Water";
+        mesh.indexFormat = IndexFormat.UInt32;
+
+        float halfLength = planeLength * 0.5f;
+        int sideVertCount = planeLength * quadRes;
+
+        vertices = new Vector3[(sideVertCount + 1) * (sideVertCount + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+        for (int i = 0, x = 0; x <= sideVertCount; ++x) {
+            for (int z = 0; z <= sideVertCount; ++z, ++i) {
+                vertices[i] = new Vector3(((float)x / sideVertCount * planeLength) - halfLength, 0, ((float)z / sideVertCount * planeLength) - halfLength);
+                uv[i] = new Vector2((float)x / sideVertCount, (float)z / sideVertCount);
+                tangents[i] = tangent;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+
+        int[] triangles = new int[sideVertCount * sideVertCount * 6];
+
+        for (int ti = 0, vi = 0, x = 0; x < sideVertCount; ++vi, ++x) {
+            for (int z = 0; z < sideVertCount; ti += 6, ++vi, ++z) {
+                triangles[ti] = vi;
+                triangles[ti + 1] = vi + 1;
+                triangles[ti + 2] = vi + sideVertCount + 2;
+                triangles[ti + 3] = vi;
+                triangles[ti + 4] = vi + sideVertCount + 2;
+                triangles[ti + 5] = vi + sideVertCount + 1;
+            }
+        }
+
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        normals = mesh.normals;
+    }
+
+    void CreateMaterial() {
+        if (waterShader == null) return;
+        if (waterMaterial != null) return;
+
+        waterMaterial = new Material(waterShader);
+
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+        renderer.material = waterMaterial;
+    }
+
+
+    void OnEnable() {
+        CreateWaterPlane();
+        CreateMaterial();
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    void Update() {
+        waterMaterial.SetVector("_Ambient", ambient);
+        waterMaterial.SetVector("_DiffuseReflectance", diffuseReflectance);
+        waterMaterial.SetVector("_SpecularReflectance", specularReflectance);
+        waterMaterial.SetVector("_TipColor", tipColor);
+        waterMaterial.SetVector("_FresnelColor", fresnelColor);
+        waterMaterial.SetFloat("_Shininess", shininess * 100);
+        waterMaterial.SetFloat("_FresnelBias", fresnelBias);
+        waterMaterial.SetFloat("_FresnelStrength", fresnelStrength);
+        waterMaterial.SetFloat("_FresnelShininess", fresnelShininess);
+        waterMaterial.SetFloat("_TipAttenuation", tipAttenuation);
+        waterMaterial.SetFloat("_FresnelNormalStrength", fresnelNormalStrength);
+        waterMaterial.SetFloat("_SpecularNormalStrength", specularNormalStrength);
+        waterMaterial.SetInt("_UseEnvironmentMap", useTextureForFresnel ? 1 : 0);
+
+        if (useTextureForFresnel) {
+            waterMaterial.SetTexture("_EnvironmentMap", environmentTexture);
+        }
+
+        if (atmosphere != null) {
+            waterMaterial.SetVector("_SunDirection", atmosphere.GetSunDirection());
+            waterMaterial.SetVector("_SunColor", atmosphere.GetSunColor());
+        }
+
+        Matrix4x4 projMatrix = GL.GetGPUProjectionMatrix(cam.projectionMatrix, false);
+        Matrix4x4 viewProjMatrix = projMatrix * cam.worldToCameraMatrix;
+        waterMaterial.SetMatrix("_CameraInvViewProjection", viewProjMatrix.inverse);
+    }
+
+    void OnDisable() {
+        if (waterMaterial != null) {
+            Destroy(waterMaterial);
+            waterMaterial = null;
+        }
+
+        if (mesh != null) {
+            Destroy(mesh);
+            mesh = null;
+            vertices = null;
+            normals = null;
+        }
+    }
+
+    private void OnDrawGizmos() {
+        /*
+        if (vertices == null) return;
+
+        for (int i = 0; i < vertices.Length; ++i) {
+            Gizmos.color = Color.black;
+            Gizmos.DrawSphere(transform.TransformPoint(displacedVertices[i]), 0.1f);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(transform.TransformPoint(displacedVertices[i]), displacedNormals[i]);
+        }
+        */
+    }
+}
---
+++ Assets/Shaders/FFTWater.shader
@@ -0,0 +1,138 @@
+Shader "Custom/FFTWater" {
+		
+		Properties {
+			[Enum(Off, 0, On, 1)] _ZWrite ("Z Write", Float) = 1
+		}
+
+	SubShader {
+		Tags {
+			"LightMode" = "ForwardBase"
+		}
+
+		Pass {
+
+			ZWrite On
+
+			CGPROGRAM
+
+			#pragma vertex vp
+			#pragma fragment fp
+
+			#include "UnityPBSLighting.cginc"
+            #include "AutoLight.cginc"
+
+			struct VertexData {
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+			};
+
+			struct v2f {
+				float4 pos : SV_POSITION;
+				float3 normal : TEXCOORD1;
+				float3 worldPos : TEXCOORD2;
+			};
+
+
+			#define PI 3.14159265358979323846
+
+			float hash(uint n) {
+				// integer hash copied from Hugo Elias
+				n = (n << 13U) ^ n;
+				n = n * (n * n * 15731U + 0x789221U) + 0x1376312589U;
+				return float(n & uint(0x7fffffffU)) / float(0x7fffffff);
+			}
+
+			float3 _SunDirection, _SunColor;
+
+			float _NormalStrength, _FresnelNormalStrength, _SpecularNormalStrength;
+
+			samplerCUBE _EnvironmentMap;
+			int _UseEnvironmentMap;
+
+			float3 _Ambient, _DiffuseReflectance, _SpecularReflectance, _FresnelColor, _TipColor;
+			float _Shininess, _FresnelBias, _FresnelStrength, _FresnelShininess, _TipAttenuation;
+
+			float4x4 _CameraInvViewProjection;
+			sampler2D _CameraDepthTexture;
+
+			v2f vp(VertexData v) {
+				v2f i;
+                i.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                i.normal = normalize(UnityObjectToWorldNormal(v.normal));
+                i.pos = UnityObjectToClipPos(v.vertex);
+				
+				return i;
+			}
+
+			float3 ComputeWorldSpacePosition(float2 positionNDC, float deviceDepth) {
+				float4 positionCS = float4(positionNDC * 2.0 - 1.0, deviceDepth, 1.0);
+				float4 hpositionWS = mul(_CameraInvViewProjection, positionCS);
+				return hpositionWS.xyz / hpositionWS.w;
+			}
+
+			float4 fp(v2f i) : SV_TARGET {
+                float3 lightDir = -normalize(_SunDirection);
+                float3 viewDir = normalize(_WorldSpaceCameraPos - i.worldPos);
+                float3 halfwayDir = normalize(lightDir + viewDir);
+
+                float height = 0.0f;
+				float3 normal = normalize(i.normal);
+
+				// normal = centralDifferenceNormal(i.worldPos, 0.01f);
+				normal.xz *= _NormalStrength;
+				normal = normalize(normal);
+
+				float ndotl = DotClamped(lightDir, normal);
+
+				float3 diffuseReflectance = _DiffuseReflectance / PI;
+                float3 diffuse = _LightColor0.rgb * ndotl * diffuseReflectance;
+
+				// Schlick Fresnel
+				float3 fresnelNormal = normal;
+				fresnelNormal.xz *= _FresnelNormalStrength;
+				fresnelNormal = normalize(fresnelNormal);
+				float base = 1 - dot(viewDir, fresnelNormal);
+				float exponential = pow(base, _FresnelShininess);
+				float R = exponential + _FresnelBias * (1.0f - exponential);
+				R *= _FresnelStrength;
+				
+				float3 fresnel = _FresnelColor * R;
+
+				if (_UseEnvironmentMap) {
+					float3 reflectedDir = reflect(-viewDir, normal);
+					float3 skyCol = texCUBE(_EnvironmentMap, reflectedDir).rgb;
+					float3 sun = _SunColor * pow(max(0.0f, DotClamped(reflectedDir, lightDir)), 500.0f);
+
+					fresnel = skyCol.rgb * R;
+					fresnel += sun * R;
+				}
+
+
+				float3 specularReflectance = _SpecularReflectance;
+				float3 specNormal = normal;
+				specNormal.xz *= _SpecularNormalStrength;
+				specNormal = normalize(specNormal);
+				float spec = pow(DotClamped(specNormal, halfwayDir), _Shininess) * ndotl;
+                float3 specular = _LightColor0.rgb * specularReflectance * spec;
+
+				// Schlick Fresnel but again for specular
+				base = 1 - DotClamped(viewDir, halfwayDir);
+				exponential = pow(base, 5.0f);
+				R = exponential + _FresnelBias * (1.0f - exponential);
+
+				specular *= R;
+				
+
+
+				float3 tipColor = _TipColor * pow(height, _TipAttenuation);
+
+				float3 output = _Ambient + diffuse + specular + fresnel + tipColor;
+
+
+				return float4(output, 1.0f);
+			}
+
+			ENDCG
+		}
+	}
+}
